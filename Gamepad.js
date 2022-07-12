51186b0c54f485748ffc5604e288065cf9544d56
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-07-12 17:32:10 +0300

    Update Gamepad.js

--- Gamepad.js
+++ Gamepad.js
@@ -1,21 +1,32 @@
 
 let Gamepad = {
   
+  'listeners': {},
+  
   'controllers': {
     
     'on': {
       
-      // a, b, x, y, shoulder-left, shoulder-right, trigger-left, trigger-right, select, start, joystick-left, joystick-right, dpad-up, dpad-down, dpad-left, dpad-right, home, share
+      // a, b, x, y, shoulder-left, shoulder-right, select, start, joystick-left, joystick-right, dpad-up, dpad-down, dpad-left, dpad-right, home, share
       'press': (key, callback) => {
         
-        
+        Gamepad.listeners[key] = {
+          callback: callback,
+          lastValue: 0
+        };
         
       },
       
-      // joystick-left, joystick-right
+      // joystick-left, joystick-right, trigger-left, trigger-right
       'move': (key, callback) => {
         
-        
+        Gamepad.listeners[key] = {
+          callback: callback,
+          lastValue: {
+            x: 0,
+            y: 0
+          }
+        };
         
       }
       
@@ -25,7 +36,77 @@ let Gamepad = {
   
   'update': () => {
     
+    Gamepad.updateControllers();
     
+    
+    const controllers = Object.values(Gamepad.controllers);
+    
+    controllers.forEach(controller => {
+      
+      const buttons = controller.buttons;
+      
+      buttons.forEach((button, index) => {
+        
+        const buttonName = buttonMap[index];
+        
+        const buttonListener = Gamepad.listeners[buttonName];
+        
+        // if button listener exists
+        if (buttonListener) {
+          
+          // if the button's value changed
+          if (button.value !== buttonListener.lastValue) {
+          
+            // call button listener with button value
+            Gamepad.listeners[buttonName].callback(button.value);
+            
+          }
+
+        }
+        
+      });
+      
+      
+      const axesObj = controller.axes;
+      let axes = {};
+      
+      // map axes to object
+      axesObj.forEach((axisValue, index) => {
+        
+        const axis = axisMap[index][0];
+        const axisName = axisMap[index][1];
+        
+        axes[axisName][axis] = axisValue;
+        
+      });
+      
+      // run on all axes
+      Object.values(axes).forEach(axisName => {
+        
+        const axisListener = Gamepad.listeners[axisName];
+        
+        // if axis listener exists
+        if (axisListener) {
+          
+          const axisObj = axes[axisName];
+          
+          // if the axis' value changed
+          if (axisObj.x !== axisListener.lastValue.x ||
+              axisObj.y !== axisListener.lastValue.y) {
+            
+            // call axis listener with axis value
+            Gamepad.listeners[axisName].callback({
+              x: axisObj.x,
+              y: axisObj.y
+            });
+            
+          }
+
+        }
+        
+      });
+      
+    });
     
     
     // if controllers are connected
@@ -39,7 +120,23 @@ let Gamepad = {
     
   },
   
-  'addGamepadEvents': () => {
+  'updateControllers': () => {
+    
+    const controllers = navigator.getGamepads();
+    
+    controllers.forEach(controller => {
+      
+      if (controller && controller.index) {
+        
+        Gamepad.controllers[controller.index] = controller;
+        
+      }
+      
+    });
+    
+  },
+  
+  'addGamepadListeners': () => {
     
     window.addEventListener('gamepadconnected', (e) => {
       
@@ -80,9 +177,16 @@ let Gamepad = {
     15: 'dpad-right',
     16: 'home',
     17: 'share'
+  },
+  
+  'axesMap': {
+    0: ['x', 'joystick-left'],
+    1: ['y', 'joystick-left'],
+    2: ['x', 'joystick-right'],
+    3: ['y', 'joystick-right'],
   }
   
 };
 
-
+Gamepad.addGamepadListeners();
 
